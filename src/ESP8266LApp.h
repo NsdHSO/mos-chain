59fac2276fcbf548d03e94658a1476ea7ba40f77
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2025-04-02 10:57:08 +0300

    feat: move in separate file all things

---
+++ src/ESP8266LApp.h
@@ -0,0 +1,49 @@
+#ifndef ESP8266LAPP_H
+#define ESP8266LAPP_H
+
+#include <Arduino.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <Wire.h>
+#include <PubSubClient.h>
+#include "../include/secrets.h"
+#include <SSD1306Wire.h>
+
+class ESP8266LApp {
+private:
+    // Pin definitions
+    static const int D1 = 5;  // SCL on GPIO5
+    static const int D2 = 4;  // SDA on GPIO4
+    static const int led = LED_BUILTIN;
+
+    // Network credentials
+    const char* ssid = WIFI_SSID;
+    const char* password = WIFI_PASS;
+
+    // MQTT settings
+    const char* mqtt_server = "192.168.68.54";
+    const char* mqtt_topic = "test/t";
+    const int mqtt_port = 1883;
+    String mqtt_client_id;
+
+    // Objects
+    ESP8266WebServer server;
+    WiFiClient espClient;
+    PubSubClient client;
+    SSD1306Wire display;
+
+    // Private methods
+    void callback(char* topic, byte* payload, unsigned int length);
+    void printMQTTState(int state);
+    void reconnect();
+    void handleRoot();
+    void handleNotFound();
+    void handleUpdateCall();
+
+public:
+    ESP8266LApp();
+    void begin();
+    void run();
+};
+
+#endif
